169ac564d3a498561ea9f02a10727110a14671e9
Mari300 <jahfly9@gmail.com>
2025-01-10 16:48:17 -0500

    changed description for about page and landing

--- react-portfolio-project/src/components/pages/About.jsx
+++ react-portfolio-project/src/components/pages/About.jsx
@@ -1,9 +1,10 @@
 export default function About() {
-
     return (
     <>
         <p id="description">
-        This is my About page.
+        This is my About page. This will be the page where I display my 
+        projects and more.<br /> There will be many more to come.
+        I'll be including my technologies down below:
         </p>
     </>
     )
--- react-portfolio-project/src/components/pages/Landing.jsx
+++ react-portfolio-project/src/components/pages/Landing.jsx
@@ -13,14 +13,17 @@ return (
         <a href="https://mls-dreammakers.github.io/random-users-app/">
             {/* <div className="picture"> */}
             <img className="picture" src="/src/pictures/Random User Generator.png">
-
             </img>
             {/* </div> */}
         </a>
         </div>
             <p className="information">
-                This is information for the picture.<br />
-                Sample Picture.
+            {/* title goes here */}{/* line breaks make space for div */}
+            Random User Generator App<br />
+            This app demonstrates the use of an API<br />
+            Once refreshed, it grabs random information<br /> 
+            (email, phone number, address)<br />
+            And displays it
              </p>
       </div>
       <div className="display-container-right">
@@ -30,9 +33,9 @@ return (
             </p>
       <div className="picture-div">
         <a href="https://react-api-project.github.io/marvel-comic-react-project/">
-                <img className="picture" src="/src/pictures/Marvel React API.png">
+            <img className="picture" src="/src/pictures/Marvel React API.png">
                 
-                </img>
+            </img>
         </a>
      </div>
       </div>
