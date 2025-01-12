381822b0fe9c6c4289d0a0b12bdf52043f302375
Mari300 <jahfly9@gmail.com>
2025-01-11 21:35:02 -0500

    attempting to add glow to buttons

--- react-portfolio-project/src/components/pages/About.jsx
+++ react-portfolio-project/src/components/pages/About.jsx
@@ -1,11 +1,15 @@
 export default function About() {
     return (
     <>
-        <p id="description">
-        This is my About page. This will be the page where I display my 
-        projects and more.<br /> There will be many more to come.
-        I'll be including my technologies down below:
-        </p>
+        <div className="about-content">
+            <img className="portrait" src="/src/pictures/Professional Headshot.jpg"></img>
+                <div className="description">
+                This is my About page. This will be the page where I display my 
+                projects and more.<br /> There will be many more to come.
+                I'll be including my technologies down below:
+                <a href="https://github.com/jahmarimaxwell">jahmarimaxwell</a>
+                </div>
+        </div>
     </>
     )
 }
