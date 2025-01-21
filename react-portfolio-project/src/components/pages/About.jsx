5ff02cbea62bb1f7c0c334f9256e285572358d35
jahmarimaxwell <jahmarimaxwell@gmail.com>
2025-01-21 11:57:56 -0500

    added proper structure for about page

--- react-portfolio-project/src/components/pages/About.jsx
+++ react-portfolio-project/src/components/pages/About.jsx
@@ -4,11 +4,14 @@ export default function About() {
         <div className="about-content">
             <img className="portrait" src="/src/pictures/Professional Headshot.jpg"></img>
                 <div className="description">
-                This is my About page. This will be the page where I display my 
-                projects and more.<br /> There will be many more to come.
-                I'll be including my technologies down below:
-                <a href="https://github.com/jahmarimaxwell">jahmarimaxwell</a>
+                    <div className="about-information">
+                    This is my About page. This will be the page where I display my 
+                    projects and more.<br /> There will be many more to come.
+                    I'll be including my technologies down below:
+                    </div>
+                <a className="github-link" href="https://github.com/jahmarimaxwell">jahmarimaxwell</a>
                 </div>
+
         </div>
     </>
     )
