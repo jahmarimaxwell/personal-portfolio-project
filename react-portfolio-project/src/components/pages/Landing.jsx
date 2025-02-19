97a3bcfac58b05cd16acf3713e878483dfef3717
Mari300 <jahfly9@gmail.com>
2025-02-19 14:03:31 -0500

    added the running game gif and description draft

--- react-portfolio-project/src/components/pages/Landing.jsx
+++ react-portfolio-project/src/components/pages/Landing.jsx
@@ -12,14 +12,14 @@ return (
                 <img className="picture" src="\src\gifs\Random User Generator App.gif"></img>
             </div>
             <div className="content">
-                <p className="information">
+                <div className="information">
                 {/* title goes here */}{/* line breaks make space for div */}
                 <strong><h2 className="project-title">Random User Generator App</h2></strong><br />
                 This app demonstrates the use of an API<br />
                 Once refreshed, it grabs random information<br /> 
                 (email, phone number, address)<br />
                 And displays it
-                </p>
+                </div>
                 <button className="landing-button">
                     <a href="https://mls-dreammakers.github.io/random-users-app/">
                         Click to view
@@ -29,7 +29,7 @@ return (
       </div>
       <div className="display-container-right">
         <div className="content">
-            <p className="information">
+            <div className="information">
                {/* title goes here */}{/* line breaks make space for div */}
                <strong><h2 className="project-title">Marvel World</h2></strong><br />
                This App demonstrates the use of <strong>React</strong><br />
@@ -39,7 +39,7 @@ return (
                as well their description. It also allows you <br />
                to be able to see which comic issue < br /> 
                they appear in
-            </p>
+            </div>
             <button className="landing-button">
                 <a href="https://github.com/React-API-Project/marvel-comic-react-project">
                     Click to view
@@ -52,14 +52,22 @@ return (
       </div>
       <div className="display-container-left">
             <div className="picture-div">
-                <div className="picture">
-                
-                </div>
+                <img className="picture" src="\src\gifs\The Running Game.gif"></img>
             </div>
-            <p className="information">
-                This is information for the picture.<br />
-                Sample Picture.
-             </p>
+           <div className="content">
+                <div className="information">
+                <strong><h2 className="project-title">The Running Game</h2></strong><br />
+                    A 2D Platformer aimed to replicate <br /> 
+                    old Flash Player games. This was my <br />
+                    first attempt at building a game<br />
+                    Sample Picture.
+                </div>
+                <button className="landing-button">
+                    <a href="https://github.com/React-API-Project/marvel-comic-react-project">
+                        Click to view
+                    </a>
+                </button>
+          </div>
       </div>
     </>
     )
